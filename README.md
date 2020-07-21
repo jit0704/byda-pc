03bbd6222e2d79afd58e250f14bf240bef723c6b
전일태 <foodchain@inpix.com>
2020-07-21 17:21:14 +0900

    로그인/팝업 작업 및 디렉토리 구조 변경

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 * [퍼블리싱 리스트 바로가기](https://jit0704.github.io/byda-pc/)
 * [디자인 확인](https://zeplin.io/)
+
+# 퍼블리싱 파일 확인
+CLI로 아래 명령어를 통해 다운 받거나
+```
+$ git clone https://github.com/jit0704/byda-pc.git
+```
+또는 우측 상단 Code버튼 눌러서 zip파일 다운 받으면 됩니다.
--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,32 @@
 // publishing UI javascript
 $(function(){
+  cmmnui();
   loginForm();
 });
+
+// 공통 UI
+function cmmnui () {
+
+  // 모달 팝업
+  $('.btn__modal--open').on('click', function () {
+    $(this).modal({
+      closeExisting: false,
+      clickClose: false
+    });
+    return false;
+  });
+
+  // datepicker
+  $('#datepicker').datepicker({
+    dateFormat: 'yy-mm-dd', //Input Display Format 변경
+    showMonthAfterYear: true, //년도 먼저 나오고, 뒤에 월 표시
+    yearSuffix: "년", //달력의 년도 부분 뒤에 붙는 텍스트
+    monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'], //달력의 월 부분 텍스트
+    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'], //달력의 월 부분 Tooltip 텍스트
+    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'], //달력의 요일 부분 텍스트
+    dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'] //달력의 요일 부분 Tooltip 텍스트
+  });
+}
 
 // 로그인 폼 영역 포커스
 function loginForm () {
